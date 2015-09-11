0e3f801612bbc77fea5b522f34c9f0e03ee40e22
Dean McDonnell <mcdonnelldean@outlook.com>
2015-09-11 12:53:54 +0100

    updated tests to use lab, removed redundant code, general tidy

--- test/jsonfile.test.js
+++ test/jsonfile.test.js
@@ -1,61 +1,30 @@
 /* Copyright (c) 2013-2014 Richard Rodger, MIT License */
-/*jslint node: true, asi: true */
-/*globals describe, it */
-"use strict";
 
+"use strict"
 
-var seneca = require('seneca')
-var shared = require('seneca-store-test')
-var assert = require('assert')
+var Seneca = require('seneca')
+var Lab = require('lab')
+var CommonTests = require('seneca-store-test')
 
+var lab = exports.lab = Lab.script()
+var describe = lab.describe
+var it = lab.it
 
-
-var si = seneca({log:'silent'})
-si.use('..',{
+var seneca = Seneca({log:'silent'})
+seneca.use('..', {
   folder: __dirname + '/db'
 })
 
-si.__testcount = 0
 var testcount = 0
+seneca.__testcount = 0
 
-
-describe('jsonfile', function(){
-  it('basic', function(done){
+describe('JSON File Store', function () {
+  it('Common Tests', function (done) {
     testcount++
-    shared.basictest(si,done)
+    CommonTests.basictest(seneca, done)
   })
 
-  it('extra', function(done){
-    testcount++
-    extratest(si,done)
-  })
-
-  it('save with passing an id$', function(done) {
-
-    var product = si.make('product')
-
-    product.id$ = '12345'
-    product.name = 'pear'
-
-    si.act(
-      { role:'entity', cmd:'save', ent: product},
-      function( err, product ) {
-        assert(!err)
-        assert.equal(product.id, '12345')
-        done()
-      })
-  })
-
-  it('close', function(done){
-    shared.closetest(si,testcount,done)
+  it('Additional Tests', function (done) {
+    CommonTests.closetest(seneca, testcount, done)
   })
 })
-
-
-
-function extratest(si,done) {
-  console.log('EXTRA')
-  si.__testcount++
-  done()
-}
-
